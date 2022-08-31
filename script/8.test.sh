e024c5b9779dbbc81329a1ff2fea9f1e4f49975a
DreamingFuture <739292435@qq.com>
2022-08-31 10:22:11 +0000

    unimportant updates

--- script/8.test.sh
+++ script/8.test.sh
@@ -1,7 +1,7 @@
-
-for task_name in 'trec' 'agnews' 'mrpc' 'sst2' 'snli' 'yelpp'
+# 'trec'
+for task_name in 'agnews' 'mrpc' 'sst2' 'snli' 'yelpp'
 do
 echo "===== task: $task_name start! ====="
-python deep_test.py  --task_name $task_name --device "cuda:5" --batch_size 4
+python deep_test.py  --task_name $task_name --device "cuda:2" --batch_size 4
 echo "===== task: $task_name finished! ====="
 done
